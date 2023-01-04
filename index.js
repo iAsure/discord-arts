f00787180aa9762c24052f7bde653e94fbec13c8
iAsure <asuregames01@gmail.com>
2023-01-04 15:38:37 -0600

    index fix

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 module.exports = {
   profileImage: require('./src/Code/profileImage'),
 };
-
-const profileImage = require("./src/Code/profileImage")
-const fs = require("fs")
-
-async function genImg(){
-const buffer = await profileImage("339919990947971105")
-fs.writeFileSync("test.png", buffer)
-
-}
-
-genImg()
