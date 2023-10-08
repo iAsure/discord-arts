28698f44ca9e7dfbfc33d3f8bdd604b14ee538c5
iAsure <asuregames01@gmail.com>
2023-10-08 02:44:22 -0600

    0.5.5

--- src/cards/profile-image.js
+++ src/cards/profile-image.js
@@ -16,7 +16,7 @@ async function profileImage(user, options) {
 
   try {
     // Private and exclusive Discord-Arts API
-    const userData = await fetch(`http://172.93.101.28:8242/user/${user}`);
+    const userData = await fetch(`https://discord-arts.asure.dev/user/${user}`);
     const json = await userData.json();
     data = json.data;
   } catch (error) {
